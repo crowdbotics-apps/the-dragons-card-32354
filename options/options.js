862c9ec7b3f9ff1441a1e0bc3a073bf7e83b5e0d
Crowdbotics <team@crowdbotics.com>
2021-12-11 19:02:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_dragons_card_32354",
+  url: "https://the_dragons_card_32354.botics.co",
+  api: "https://the_dragons_card_32354.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
